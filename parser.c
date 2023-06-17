bfcb33f048b90b0f203093b476f50101061d2a2a
“marylishop” <marylishop@yahoo.com>
2023-06-17 21:23:29 +0400

    update for files

--- parser.c
+++ parser.c
@@ -9,25 +9,24 @@
  */
 command_t *_parser_cmd(char *myself, char *cmd_line)
 {
-	size_t i = 0;
-	char *cmd_str = NULL;
-	const char *arg_sep = " \n";
-	command_t *cmd_node = NULL;
+size_t i = 0;
+char *cmd_str = NULL;
+const char *arg_sep = " \n";
+command_t *cmd_node = NULL;
 
-	cmd_str = strtok(cmd_line, arg_sep); /* Get the first token */
-	if (cmd_str)
-	{
-		cmd_node = new_cmd_node(myself);
-		add_tok_to_cmd(myself, cmd_node, i, cmd_str);
+cmd_str = strtok(cmd_line, arg_sep); /* Get the first token */
+if (cmd_str)
+{
+cmd_node = new_cmd_node(myself);
+add_tok_to_cmd(myself, cmd_node, i, cmd_str);
 
-		while (cmd_str != NULL)
-		{
-			cmd_str == NULL ? i : i++;
-			cmd_str = strtok(NULL, arg_sep);
-			if (cmd_str)
-				add_tok_to_cmd(myself, cmd_node, i, cmd_str);
-		}
-	}
-
-	return (cmd_node);
+while (cmd_str != NULL)
+{
+cmd_str == NULL ? i : i++;
+cmd_str = strtok(NULL, arg_sep);
+if (cmd_str)
+add_tok_to_cmd(myself, cmd_node, i, cmd_str);
+}
+}
+return (cmd_node);
 }
