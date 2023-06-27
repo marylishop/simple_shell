d643b3fb4889c27b3608b51c7bb6ca3ad78073ff
“marylishop” <marylishop@yahoo.com>
2023-06-27 08:25:35 -0700

    realloc

--- shell.h
+++ shell.h
@@ -137,6 +137,7 @@ char **strtow2(char *, char);
 /* toem_realloc.c */
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_realloc(void *, unsigned int, unsigned int);
 /* toem_memory.c */
 int bfree(void **);
