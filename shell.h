9858d3904b884a3c1ee490e7ee9720e8815ef82b
Fatimazohra95 <fatimazohra32@gmail.com>
2023-06-27 05:13:52 -0700

    Committing local changes to hsh

--- shell.h
+++ shell.h
@@ -200,7 +200,7 @@ ssize_t get_node_index(list_t *, list_t *);
 /* Toem_vars.c */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
 int replace_vars(info_t *);
+int replace_alias(info_t *);
 int replace_string(char **, char *);
 #endif /* SHELL_H */
